48662ab6a15fdd75156570b91a2323982c0f6749
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-10-29 17:51:00 +0100

    Added book a session to navbar and a back route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/pages', to: 'pages#index', as: 'pages'
+  get '/back', to: 'pages#home', as: 'back'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
